f44f4bc01089cd08576f51c0a97069ebeeb9918e
Ryan Johnson <ryanleejwebdev@gmail.com>
2021-07-06 19:28:30 -0500

    Display colors as a radial burst

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useCallback, useEffect } from 'react'
-import { csv } from 'd3'
+import { csv, arc, pie } from 'd3'
 // import { message } from './utils/message'
 // useCallback - good for adding event listeners only once
 // - arg0 - function you want to control
@@ -8,6 +8,13 @@ import './App.css'
 
 const csvUrl = 'https://gist.githubusercontent.com/curran/b236990081a24761f7000567094914e0/raw/cssNamedColors.csv'
 // const csvUrl = 'https://gist.githubusercontent.com/ryan258/e4239ed73da5e5e8cc195f9c6560c89d/raw/cssNamedColors.csv'
+
+const width = 500
+const height = 500
+const centerX = width / 2
+const centerY = height / 2
+
+const pieArc = arc().innerRadius(0).outerRadius(width)
 
 const App = () => {
   const [data, setData] = useState(null)
@@ -22,7 +29,31 @@ const App = () => {
 
   // console.log(data[0])
 
-  return data ? data.map((d, i) => <div key={i} style={{ backgroundColor: d['RGB hex value'], width: '960px', height: '4px' }}></div>) : 'Loading...'
+  const colorPie = pie().value(1)
+
+  return (
+    <svg width={width} height={height}>
+      <g transform={`translate(${centerX}, ${centerY})`}>
+        {
+          colorPie(data).map((d) => (
+            <path fill={d.data['RGB hex value']} d={pieArc(d)} />
+          ))
+
+          /* data.map((d, i) => (
+          <path
+            key={i}
+            fill={d['RGB hex value']}
+            d={pieArc({
+              startAngle: (i / data.length) * 2 * Math.PI,
+              endAngle: ((i + 1) / data.length) * 2 * Math.PI
+            })
+            }
+          />
+        )) */
+        }
+      </g>
+    </svg>
+  )
 }
 
 export default App
